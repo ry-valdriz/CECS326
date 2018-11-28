a6dde2af570ec647d203ce77a15ef69df25fd06d
ry-valdriz <ryvaldriz101@gmail.com>
2018-11-27 21:12:08 -0800

    new replace and readfile functions

--- cecs326_fork_lab.cpp
+++ cecs326_fork_lab.cpp
@@ -9,7 +9,9 @@
 
 using namespace std;
 
-void replace(string target, string replacement);
+// void replace(string& target, string& replacement);
+string readFile(ifstream& is);
+int replace(string& fileContent, const string& target, const string& replacement);
 
 int main()
 {
@@ -39,10 +41,13 @@ int main()
         cout << "Target: " << target << endl;
         cout << "Replacement: " << replacement << endl;
         int count = 0;
-        /**
-         * Enter replacement algorithm
-         *
-         **/
+
+        ifstream read_file("articleTester.txt");
+        ofstream write_file("articleTester.txt");
+
+        //replace(target, replacement); //old replace function
+        String fileContent = readFile(read_file);
+        count = replace(fileContent, target, replacement);
 
         cout << "Count: " << count << endl;
         //exit();//end of child process
@@ -59,38 +64,65 @@ int main()
 
     } //end of while loop
 
-    cout << "BYE BYE!!!" << endl;
+    cout << "" << endl;
 
 }//end of main
 
 
-void replace(string target, string replacement){
-    std::ifstream read_article;
-    read_article.open("article.txt");
-
-    std::ofstream write_article;
-    write_article.open("article.txt");
-    //article.open("article.txt", ios::out | ios::in);
-
-    if(!read_article.is_open()){
-        cout << "article was not opened" << endl;
-        return;
-    }
-
-    string line;
-
-    while(getline(read_article,line)){
-      size_t pos = line.find(target);
-      if (pos != std::string::npos){
-        line.replace(pos, target.length(), replacement);
-      }
-    }
-
-    // while(article >> word){
-    //     if(word == target){
-    //
-    //     }
-    // }
-
-
+string readFile(ifstream& inputStream){
+  string content;
+  for(char ch; inputStream.get(ch); content.push_back(ch)){}
+  return content;
 }
+
+int replace(string& file_content, const string& target, const string& replacement){
+  int count = 0; //counter for # of replacements
+
+  auto pos = file_content.find(target);
+  while( pos != std::string::npos){
+    file_content.replace(pos,target.length(), replacement);
+    //search for next replacement
+    pos = file_content.find(target, pos);
+    //increment for each word you find
+    count++;
+  }
+
+  return count;
+}
+
+
+
+
+// void replace(string& target, string& replacement){
+//     ifstream read_article;
+//     read_article.open("articleTester.txt");
+//
+//     ofstream write_article;
+//     write_article.open("articleTester.txt");
+//     //article.open("article.txt", ios::out | ios::in);
+//
+//     if(!read_article.is_open()){
+//         cout << "article was not opened" << endl;
+//         return;
+//     }
+//
+//     string line;
+//
+//     while(getline(read_article,line)){//read the file line by line
+//       size_t pos = line.find(target); //find position of first occurence of word
+//
+//       if (pos != std::string::npos){
+//         line.replace(pos, target.length(), replacement); //replace word
+//       }
+//
+//       write_article << line << '\n';
+//     }
+//
+//     write_article.close();
+//     read_article.close();
+//     // while(article >> word){
+//     //     if(word == target){
+//     //
+//     //     }
+//     // }
+// }
